a376d7a028cef970103217c359926617ac606a4e
Alexander Anthis <anthisalex@gmail.com>
2021-10-18 13:38:46 -0700

    usual commit

---
+++ Projects/2021-10-18 - Test/test.py
@@ -0,0 +1,14 @@
+def super_cool_function():
+    a = 4
+    b = 65
+    c = 8
+    d = 91
+    return [a, b, c, d]
+
+
+def main():
+    cool_array = super_cool_function()
+    print(cool_array[3])
+
+
+main()
