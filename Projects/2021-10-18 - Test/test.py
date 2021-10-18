cbee73cfe273aa51d08b3500a6bdd0600c2d678d
Alexander Anthis <anthisalex@gmail.com>
2021-10-18 13:45:34 -0700

    Added comments to example code

--- Projects/2021-10-18 - Test/test.py
+++ Projects/2021-10-18 - Test/test.py
@@ -1,13 +1,19 @@
 def super_cool_function():
+    # Define some integer variables
     a = 4
     b = 65
     c = 8
     d = 91
+
+    # Return an array of the variables
     return [a, b, c, d]
 
 
 def main():
+    # Run the function, then save result as "cool_array"
     cool_array = super_cool_function()
+
+    # Print the 4th variable in the array
     print(cool_array[3])
 
 
